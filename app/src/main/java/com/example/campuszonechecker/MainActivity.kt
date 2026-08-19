c13d25c77ecc5e01b9c91242193e4827057ddfe3
shamini-03 <shaminilak03@gmail.com>
2026-08-19 09:12:06 +0530

    create main project and add dependencies.

---
+++ app/src/main/java/com/example/campuszonechecker/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.campuszonechecker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    // Fixed reference location for our Campus/Home Zone
+    companion object {
+
+        // Reference latitude
+        const val REFERENCE_LATITUDE = 6.9271
+
+        // Reference longitude
+        const val REFERENCE_LONGITUDE = 79.8612
+
+        // Zone radius in meters
+        const val ZONE_RADIUS_METERS = 200f
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById(R.id.main)
+        ) { v, insets ->
+
+            val systemBars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+            )
+
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+
+            insets
+        }
+    }
+}
